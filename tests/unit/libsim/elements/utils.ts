5b77d026b6c5231a60a472a3ed2ed1b88c76a11a
Alexey Ermakov <fufler@gmail.com>
2021-09-24 20:51:59 +0300

    Improve makeSpec: allow usage of predefined input signals and custom arguments formatter

--- tests/unit/libsim/elements/utils.ts
+++ tests/unit/libsim/elements/utils.ts
@@ -10,8 +10,13 @@ export type DeviceProvider = ((engine: Engine) => Device)
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type DeviceImplementationFunction = (input: any) => any
 type Device2In1OutImplementationFunction = (inA: Signal, inB: Signal) => Signal
+type MultiPinSignals = Array<Array<Signal>>
+type NamedMultiPinSignal = Record<string, Signal>
+type NamedMultiPinSignals = Array<NamedMultiPinSignal>
 
-export const makeSignals = (n: number): Array<Array<Signal>> => {
+type SignalFormatter = (signal: NamedMultiPinSignal) => string
+
+export const makeSignals = (n: number): MultiPinSignals => {
   if (n === 1) {
     return [[Signal.LOW], [Signal.HIGH]]
   }
@@ -28,15 +33,70 @@ export const SIGNALS = makeSignals(1)
 export const SIGNALS2 = makeSignals(2)
 export const SIGNALS3 = makeSignals(3)
 
-const objectToString = (obj: unknown): string => _(obj)
+const pinsToString = (signal: NamedMultiPinSignal): string => _(signal)
   .toPairs()
   .map(([k, v]) => `${k} = ${v}`)
   .join(', ')
 
+export const toPins = (prefix: string, value: number): Record<string, Signal> => {
+  const pins: Record<string, Signal> = {}
+
+  for (let i = 0; i < 16; i++) {
+    pins[`${prefix}${i}`] = value % 2
+
+    value = Math.floor(value / 2)
+  }
+
+  return pins
+}
+
+export const fromPins = (pins: Record<string, Signal>, prefix: string): number => _(pins)
+  .toPairs()
+  .filter(([name]) => name.startsWith(prefix))
+  .reduce(
+    (acc, [name, signal]) => acc + signal * (2 ** parseInt(name.substring(prefix.length), 10)),
+    0
+  )
+
+export const groupByPrefixFormatter = (re: RegExp, indexed: boolean): SignalFormatter => (signal: NamedMultiPinSignal): string => {
+  const matched: Array<[string, string, number | string]> = []
+
+  for (const key of _.keys(signal)) {
+    const m = key.match(re)
+
+    if (m == null) {
+      continue
+    }
+
+    const name = m[0]
+    const group = m[1]
+    const index = m[2]
+
+    if (name != null && group != null && index != null) {
+      matched.push([name, group, index ? parseInt(index, 10) : index])
+    }
+  }
+
+  return _(matched)
+    .groupBy(m => m[1])
+    .mapValues(gm =>
+      _(gm)
+        .sortBy(m => m[2])
+        .map(m => m[0])
+        .reduce(
+          (acc, f) => signal[f] + acc,
+          ''
+        )
+    )
+    .toPairs()
+    .map(([k, v]) => `${k} = ${v}`)
+    .join(', ')
+}
+
 export const wrap2In1Out = (impl: Device2In1OutImplementationFunction): DeviceImplementationFunction =>
   ({ inA, inB }) => ({ out: impl(inA, inB) })
 
-export const makeSpec = (name: string, provider: DeviceProvider, impl: DeviceImplementationFunction): Suite => describe(name, () => {
+export const makeSpec = (name: string, provider: DeviceProvider, impl: DeviceImplementationFunction, customSignals?: NamedMultiPinSignals, signalFormatter?: SignalFormatter): Suite => describe(name, () => {
   const engine = new Engine()
 
   const device = provider(engine)
@@ -56,7 +116,11 @@ export const makeSpec = (name: string, provider: DeviceProvider, impl: DeviceImp
     }
   }
 
-  for (const signals of makeSignals(inputPins.length)) {
+  const inputSignals: MultiPinSignals = customSignals?.map(sigs => inputPins.map(p => sigs[p.name])) ?? makeSignals(inputPins.length)
+
+  const formatter = signalFormatter ?? pinsToString
+
+  for (const signals of inputSignals) {
     const input = _(inputPins)
       .map((p, idx) => [p.name, signals[idx]])
       .fromPairs()
@@ -64,7 +128,7 @@ export const makeSpec = (name: string, provider: DeviceProvider, impl: DeviceImp
 
     const expectedOut = impl(input)
 
-    it(`${name}(${objectToString(input)}) === (${objectToString(expectedOut)})`, async () => {
+    it(`${name}(${formatter(input)}) === (${formatter(expectedOut)})`, async () => {
       for (let i = 0; i < signals.length; i++) {
         inputPins[i].setSignal(signals[i])
       }
@@ -72,7 +136,7 @@ export const makeSpec = (name: string, provider: DeviceProvider, impl: DeviceImp
       expect(await engine.run()).to.be.true
 
       for (const pin of outputPins) {
-        expect(pin.getSignal()).to.be.equal(expectedOut[pin.name])
+        expect(pin.getSignal()).to.be.equal(expectedOut[pin.name], pin.name)
       }
     })
   }
