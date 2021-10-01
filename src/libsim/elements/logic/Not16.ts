fb888ef323422f2a6ca0039594732a7e38fc9464
Alexey Ermakov <fufler@gmail.com>
2021-10-01 22:39:25 +0300

    Add Not16 device

---
+++ src/libsim/elements/logic/Not16.ts
@@ -0,0 +1,42 @@
+import { CompoundDevice, Device, DevicePins, inBus, outBus } from '@/libsim/Devices'
+import { Engine } from '@/libsim/Engine'
+
+import _ from 'lodash'
+import { Not } from '@/libsim/elements/logic/Not'
+import { Bus16 } from '@/libsim/Buses'
+
+export class Not16 extends CompoundDevice {
+  private readonly in: Bus16
+  private readonly out: Bus16
+  private readonly nots: Array<Not>
+
+  constructor (engine: Engine, name: string, device?: Device) {
+    super(engine, name, device)
+
+    this.in = new Bus16(engine, 'in', this)
+    this.out = new Bus16(engine, 'out', this)
+
+    this.nots = _.times(16, n => new Not(engine, `not-${n + 1}`, this))
+
+    engine.linkBuses(
+      this.in,
+      this.nots.map(a => a.in)
+    )
+
+    engine.linkBuses(
+      this.nots.map(a => a.out),
+      this.out
+    )
+  }
+
+  getDevices (): Array<Device> {
+    return this.nots
+  }
+
+  getPins (): DevicePins {
+    return [
+      ...inBus(this.in),
+      ...outBus(this.out)
+    ]
+  }
+}
---
+++ tests/unit/libsim/elements/logic/not16.spec.ts
@@ -0,0 +1,20 @@
+import { Engine } from '@/libsim/Engine'
+import { Signal } from '@/libsim/Pins'
+import { fromPins, groupByPrefixFormatter, makeSpec, randomNumber16, toPins } from '../utils'
+
+import _ from 'lodash'
+import { Not16 } from '@/libsim/elements/logic/Not16'
+
+const RANDOM_INPUT = _.times(10, randomNumber16)
+
+makeSpec(
+  'Not16',
+  (engine: Engine) => new Not16(engine, 'Not16'),
+  (input: Record<string, Signal>) => {
+    const a = fromPins(input, 'in-')
+
+    return toPins('out-', 0xFFFFFFFF - a)
+  },
+  RANDOM_INPUT.map(a => toPins('in-', a)),
+  groupByPrefixFormatter(/(in|out)-(\d)/, true)
+)
